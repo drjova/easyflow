b83f2608ecf438cd109844db8c3142112b93ba9c
Harris Tzovanakis <charalampos.tzovanakis@cern.ch>
2013-08-03 16:11:16 +0200

    Workflows with templates

--- applications/easyflow/controllers/workflow.py
+++ applications/easyflow/controllers/workflow.py
@@ -2,15 +2,14 @@
 
 service = Service()
 
-@auth.requires_login()
 def index():
   records = db(db.workflow.dataowner==auth.user_id).select()
   if len(records) > 0:
     records = records
   else:
     records = 'No records yet'
-  templateFile = os.path.join(paths.get_prog_root(), 'templates', 'view.html')
-  return dict(title='Workflows',records=records,app='workflows',tem=templateFile)
+  
+  return dict(title='Workflows',records=records,app='workflows')
 
 
 @auth.requires_login()
@@ -20,20 +19,17 @@ def create():
         response.flash = 'new record inserted'
     return dict(form=form)
 
-@auth.requires_login()
+
 def view():
-     records = db(db.workflow.dataowner==auth.user_id).select()
+     records = db(db.workflow.dataowner==2).select()
      return dict(records=records)
 
-@auth.requires_login()
+
 def single():
    record = db.workflow(request.args(0)) or redirect(URL('view'))
-   form = SQLFORM(db.workflow, record)
-   if form.process().accepted:
-       response.flash = 'form accepted'
-   elif form.errors:
-       response.flash = 'form has errors'
-   return dict(form=form)
+
+  
+   return dict(records=record)
 def edit():
       return 'Hello'
 def user(): return dict(form=auth())
