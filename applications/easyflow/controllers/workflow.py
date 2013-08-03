625e86b9e5f3a56ec6ad931dc1eff49ac95c0dcb
Nikos Kasioumis <nikos.kasioumis@cern.ch>
2013-08-03 12:41:24 +0200

    More clean up

--- applications/easyflow/controllers/workflow.py
+++ applications/easyflow/controllers/workflow.py
@@ -1,18 +1,23 @@
 # coding: utf8
+
 service = Service()
+
 @auth.requires_login()
 def create():
     form = SQLFORM(db.workflow).process()
     if form.accepted:
         response.flash = 'new record inserted'
     return dict(form=form)
+
 @service.json
 def drjova(a,b):
   return a+b
+
 @auth.requires_login()
 def view():
      records = db(db.workflow.dataowner==auth.user_id).select()
      return dict(records=records)
+
 @auth.requires_login()
 def single():
    record = db.workflow(request.args(0)) or redirect(URL('view'))
@@ -22,16 +27,17 @@ def single():
    elif form.errors:
        response.flash = 'form has errors'
    return dict(form=form)
-@auth.requires_login()
 def edit():
       return 'Hello'
 def user(): return dict(form=auth())
 
 def index():
 	 redirect(URL('view'))
+
 @auth.requires_login()
 def details():
     form = SQLFORM.smartgrid(db.workflow)
     return dict(form=form)
 def data(): 
   return dict(form=crud())
+
