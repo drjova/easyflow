e94b007d5c6ba504d722bfad3f063ba50c1eabf4
dR jOv@ <charalampos.tzovanakis@cern.ch>
2013-08-03 11:13:25 +0200

    Last updates

--- applications/easyflow/controllers/workflow.py
+++ applications/easyflow/controllers/workflow.py
@@ -1,8 +1,37 @@
 # coding: utf8
-
-def view_workflows():
-    form = SQLFORM(db.workflow_types).process()
+service = Service()
+@auth.requires_login()
+def create():
+    form = SQLFORM(db.workflow).process()
     if form.accepted:
         response.flash = 'new record inserted'
-    records = SQLTABLE(db().select(db.workflow_types.ALL),headers='fieldname:capitalize')
-    return dict(form=form, records=records)
+    return dict(form=form)
+@service.json
+def drjova(a,b):
+  return a+b
+@auth.requires_login()
+def view():
+     records = db(db.workflow.dataowner==auth.user_id).select()
+     return dict(records=records)
+@auth.requires_login()
+def single():
+   record = db.workflow(request.args(0)) or redirect(URL('view'))
+   form = SQLFORM(db.workflow, record)
+   if form.process().accepted:
+       response.flash = 'form accepted'
+   elif form.errors:
+       response.flash = 'form has errors'
+   return dict(form=form)
+@auth.requires_login()
+def edit():
+      return 'Hello'
+def user(): return dict(form=auth())
+
+def index():
+	 redirect(URL('view'))
+@auth.requires_login()
+def details():
+    form = SQLFORM.smartgrid(db.workflow)
+    return dict(form=form)
+def data(): 
+  return dict(form=crud())
--- applications/easyflow/models/workflow.py
+++ applications/easyflow/models/workflow.py
@@ -1,7 +1,51 @@
 # coding: utf8
+from gluon.tools import Auth
+auth = Auth(db)
+auth.define_tables()
+from gluon.tools import Crud
+crud = Crud(db)
+
 db.define_table(
-        'workflow_types',
+        'workflow',
         Field('id'),
         Field('name'),
         Field('description'),
+        Field('dataowner', db.auth_user, default=auth.user.id if auth.user
+        else None, writable=False, readable=False),
         format = '%(name)s')
+db.define_table(
+        'status',
+        Field('id'),
+        Field('name'),
+        Field('description'),
+        Field('order_to_workflow'),
+        Field('workflow_id', db.workflow),
+        Field('dataowner', db.auth_user, default=auth.user.id if auth.user
+        else None, writable=False, readable=False),
+        format = '%(name)s')
+db.define_table(
+        'details',
+        Field('id'),
+        Field('name'),
+        Field('description'),
+        Field('order_to_status'),
+        Field('status_id', db.status),
+        Field('dataowner', db.auth_user, default=auth.user.id if auth.user
+        else None, writable=False, readable=False),
+        format = '%(name)s')
+db.define_table(
+        'instances',
+        Field('id'),
+        Field('workflow_id',db.workflow),
+        Field('active_status_id',db.status),
+        format = '%(name)s')
+db.define_table(
+        'instances_details',
+        Field('id'),
+        Field('detail_id',db.details),
+        Field('instance_id',db.instances),
+        Field('instanceValue'),
+        format = '%(name)s')
+
+db.workflow.name.requires = IS_NOT_EMPTY()
+db.workflow.description.requires = IS_NOT_EMPTY()
